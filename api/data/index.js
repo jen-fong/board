7d7e515fa42c1f77419575725ca644cbf6a17073
Jenny Fong <jennyfong1015@gmail.com>
2021-01-29 21:15:31 -0500

    Add in memory data storage

---
+++ api/data/index.js
@@ -0,0 +1,27 @@
+// in memory data storage
+module.exports = {
+  // prepopulated dummy data channels
+  channels: [
+    {
+      id: 1,
+      name: "General",
+    },
+    {
+      id: 2,
+      name: "Music",
+    },
+    {
+      id: 3,
+      name: "Food",
+    },
+    {
+      id: 4,
+      name: "Travel",
+    },
+    {
+      id: 5,
+      name: "Pets",
+    },
+  ],
+  messages: [],
+};
